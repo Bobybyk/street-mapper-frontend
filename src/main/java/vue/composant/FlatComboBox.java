e250a012c2933fce4424144a5bc21fc7784d3fd1
david <david2maykel@gmail.com>
2023-04-19 10:02:11 +0200

    changement de l'éditeur du JComboBox

--- src/main/java/vue/composant/FlatComboBox.java
+++ src/main/java/vue/composant/FlatComboBox.java
@@ -3,6 +3,9 @@ package vue.composant;
 import vue.utils.BuilderJComposant;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicComboBoxEditor;
+import javax.swing.plaf.basic.BasicComboBoxUI;
+import java.awt.*;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
 import java.awt.event.MouseAdapter;
@@ -12,6 +15,7 @@ public class FlatComboBox extends JComboBox<String> {
     public FlatComboBox(String[] items) {
         super(items);
         setEditable(true);
+        setEditor(new ComboBoxCustom());
         setFont(BuilderJComposant.lemontRegularFont(20));
         JTextField textField = (JTextField) getEditor().getEditorComponent();
         textField.addMouseListener(new MouseAdapter() {
@@ -34,5 +38,49 @@ public class FlatComboBox extends JComboBox<String> {
                 hidePopup();
             }
         });
+    }
+
+    private class ComboBoxCustom extends BasicComboBoxEditor {
+        private final JTextField field = new JTextField();
+
+        public ComboBoxCustom() {
+            super();
+            field.setBorder(null);
+            field.addFocusListener(new FocusAdapter() {
+                public void focusGained(FocusEvent e) {
+                    if (field.getText().length() > 0) field.selectAll();
+                }
+            });
+            setUI(new BasicComboBoxUI() {
+                @Override
+                protected JButton createArrowButton() {
+                    return new JButton() {
+                        @Override
+                        public int getWidth() {
+                            return 0;
+                        }
+                    };
+                }
+            });
+        }
+
+        @Override
+        public Component getEditorComponent() {
+            return this.field;
+        }
+
+        @Override
+        public Object getItem() {
+            return this.field.getText();
+        }
+
+        @Override
+        public void setItem(Object item) {
+            String text = (item != null) ? item.toString() : "";
+            this.field.setText(text);
+            this.field.setSelectionStart(0);
+            this.field.setSelectionEnd(text.length());
+        }
+
     }
 }
