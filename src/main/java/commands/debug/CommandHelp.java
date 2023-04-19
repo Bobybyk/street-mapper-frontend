37135de00f65c4fd15d927c6f0c567ce436c67c5
Matthieu Le Franc <matt.lefranc@gmail.com>
2023-04-19 15:43:28 +0200

    Revert "ajout des nouvelles commandes dans help"
    This reverts commit e0998e3c1590b0942ea9fec64ab3db87c5bc88eb.
    
    	modifié :         src/main/java/commands/debug/CommandHelp.java

--- src/main/java/commands/debug/CommandHelp.java
+++ src/main/java/commands/debug/CommandHelp.java
@@ -6,27 +6,22 @@ public class CommandHelp implements CommandDebug {
 
     @Override
     public void execute(String[] args, Console console) {
-        System.out.println("#########################################################");
-        System.out.println("##                                                     ##");
-        System.out.println("##                 Console de debogage                 ##");
-        System.out.println("##                                                     ##");
-        System.out.println("##-----------------------------------------------------##");
-        System.out.println("##                                                     ##");
-        System.out.println("##                  COMMANDES LOCALES                  ##");
-        System.out.println("##                                                     ##");
-        System.out.println("## kill : ferme le programme                           ##");
-        System.out.println("## help : affiche ce menu de commandes                 ##");
-        System.out.println("##                                                     ##");
-        System.out.println("##                  COMMANDES RESEAU                   ##");
-        System.out.println("##                                                     ##");
-        System.out.println("## ROUTE <addr1> <addr2>                               ##");
-        System.out.println("##      -> demande le chemin de <addr1> à <addr2>      ##");
-        System.out.println("## TIME <station> <hour> <minutes>                     ##");
-        System.out.println("##      -> demande temps d'attente pour <station>      ##");
-        System.out.println("## SEARCH <station>                                    ##");
-        System.out.println("##      -> demande les lignes pour <station>           ##");
-        System.out.println("##                                                     ##");
-        System.out.println("#########################################################");
+        System.out.println("############################################");
+        System.out.println("##                                        ##");
+        System.out.println("##          Console de debogage           ##");
+        System.out.println("##                                        ##");
+        System.out.println("##----------------------------------------##");
+        System.out.println("##                                        ##");
+        System.out.println("##           COMMANDES LOCALES            ##");
+        System.out.println("##                                        ##");
+        System.out.println("## kill : ferme le programme              ##");
+        System.out.println("## help : affiche ce menu de commandes    ##");
+        System.out.println("##                                        ##");
+        System.out.println("##           COMMANDES RESEAU             ##");
+        System.out.println("##                                        ##");
+        System.out.println("## ROUTE <addr1> <addr2> : demande trajet ##");
+        System.out.println("##                                        ##");
+        System.out.println("############################################");
     }
 
 }
