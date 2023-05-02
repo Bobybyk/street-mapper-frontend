2320f3ffc1b0c642b1053f7ce36bbb9489687868
david <david2maykel@gmail.com>
2023-05-03 01:05:15 +0200

    factorisation et modification des requetes tcp builder

--- src/main/java/commands/tcp/RequestTcp.java
+++ src/main/java/commands/tcp/RequestTcp.java
@@ -1,11 +1,30 @@
 package commands.tcp;
 
+import console.Debug;
+import console.DebugList;
+
 public interface RequestTcp {
 
     /**
      * @param args arguments de la requête
      * @return la requête construite pour le serveur
-     * @throws ArrayIndexOutOfBoundsException
      */
-    public String commandBuilder(String[] args) throws ArrayIndexOutOfBoundsException;
+    String commandBuilder(String[] args) throws ArrayIndexOutOfBoundsException;
+
+    /**
+     * teste si la date passée en argument est un format accepté par l'objet DateFormat
+     *
+     * @param date la date à tester
+     * @return true si date est au bon format, false sinon
+     */
+    default boolean isTimeFormat(String date) {
+        try {
+            Integer.parseInt(date.substring(0, 2));
+            Integer.parseInt(date.substring(3, 5));
+            return date.charAt(2) == ':';
+        } catch (NumberFormatException e) {
+            Debug.print(DebugList.WARNING, "[WARNING/RequestTcpRoute] La date renseignée n'est pas au bon format");
+        }
+        return false;
+    }
 }
--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,66 +1,62 @@
 package controller;
 
 import app.server.data.SuggestionStations;
-import client.Client;
-import commands.tcp.RequestIndexesList;
-import utils.Observable;
+import console.Console;
 
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.Date;
 
 public class Controller {
 
-    private final Client client;
+    private final Console console;
 
-    public Controller(Client client){
-        this.client = client;
+    private final String separator = ";";
+
+    public Controller(Console console) {
+        this.console = console;
     }
 
     /**
-     *
      * Fonction dans le controller qui permet d'envoyer un requete de route au serveur
      *
-     * @param depart String de la station de depart
-     * @param arrive String de la station d'arrivé
-     * @param typeTrajet String type de trajet -> distance ou entemps
+     * @param depart       String de la station de depart
+     * @param arrive       String de la station d'arrivé
+     * @param typeTrajet   String type de trajet -> distance ou entemps
      * @param sectionAPied Boolean section à pied
-     * @param date date du trajet
+     * @param date         date du trajet
      */
     public void sendRequestRoute(String depart, String arrive, String typeTrajet, boolean sectionAPied, Date date) {
         LocalTime time = date.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalTime();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         String formattedTime = time.format(formatter);
-        String arguments = "ROUTE;"+depart+";"+arrive+";"+ formattedTime+";"+ typeTrajet+";"+ (sectionAPied ? "FOOT\n" :"\n");
-        client.setNextRequest(arguments, RequestIndexesList.ROUTE);
+        String arguments = "ROUTE;" + depart + ";" + arrive + ";" + formattedTime + ";" + typeTrajet + ";" + (sectionAPied ? "FOOT" : "");
+        console.handleCommand(arguments, separator);
     }
 
     /**
-     *
      * Fonction dans le controller qui permet d'envoyer un requete de search au serveur
      *
-     * @param word String du mot d'entrée de l'utilisateur dans la textfield
+     * @param word   String du mot d'entrée de l'utilisateur dans la textfield
      * @param depart Savoir quel type d'input l'utilisateur va cliquer
      */
     public void sendRequestSearch(String word, SuggestionStations.SuggestionKind depart) {
-        String requete = "SEARCH;" + word + ";" + depart;
-        client.setNextRequest(requete, RequestIndexesList.SEARCH);
+        String arguments = "SEARCH;" + word + ";" + depart;
+        console.handleCommand(arguments, separator);
     }
 
     /**
-     *
      * Fonction dans le controller qui permet d'envoyer un requete de Horaire au serveur
      *
      * @param station String de la station
-     * @param date date d'horaire
+     * @param date    date d'horaire
      */
     public void sendRequestHoraire(String station, Date date) {
         LocalTime time = date.toInstant().atZone(java.time.ZoneId.systemDefault()).toLocalTime();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
         String formattedTime = time.format(formatter);
-        String arguments = "TIME;"+station+";"+ formattedTime+"\n";
-        client.setNextRequest(arguments, RequestIndexesList.TIME);
+        String arguments = "TIME;" + station + ";" + formattedTime;
+        console.handleCommand(arguments, separator);
     }
 
 }
